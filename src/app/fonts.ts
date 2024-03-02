a1d082b729d600488d21739da4d6ecb07bf26dbb
Raj <raj.pulapakura@gmail.com>
2024-03-03 05:55:02 +1100

    edited metadata

---
+++ src/app/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Rock_Salt } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const rockSalt = Rock_Salt({ weight: "400", subsets: ["latin"] });
