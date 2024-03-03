9e70b6c3c3f2291a3e2511f8b746070dd5c77aba
Raj <raj.pulapakura@gmail.com>
2024-03-03 21:14:37 +1100

    skills padding

--- src/sections/skills/index.tsx
+++ src/sections/skills/index.tsx
@@ -7,7 +7,7 @@ import GridXLarge from "./components/GridXLarge";
 
 export default function Skills() {
   return (
-    <section className="px-5 md:px-10 lg:px-16 py-10 md:py-14 lg:py-16">
+    <section className="px-5 md:px-10 lg:px-16 py-10 md:py-24 lg:py-32 ">
       <div className="visible lg:hidden">
         <GridSmall />
       </div>
