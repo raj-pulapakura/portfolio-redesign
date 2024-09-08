7b9af5355b6c95685a3a223af12bf347ca44d071
Raj <raj.pulapakura@gmail.com>
2024-09-08 13:07:19 +1000

    New intro

--- src/sections/intro/components/ProfileBlock.tsx
+++ src/sections/intro/components/ProfileBlock.tsx
@@ -2,7 +2,13 @@ import { motion } from 'framer-motion';
 
 export default function ProfileBlock() {
   return (
-    <motion.div className="bg-cream rounded-lg col-span-3 p-10">
+    <motion.div
+      initial={{ opacity: 0, x: -10 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: 10 }}
+      transition={{ duration: 0.25 }}
+      className="bg-cream rounded-lg col-span-3 p-6"
+    >
       <img
         src="/photos/profile-no-bg.png"
         className="w-full h-full object-contain "
--- src/sections/intro/index.tsx
+++ src/sections/intro/index.tsx
@@ -3,10 +3,15 @@ import LinksBlock from './components/LinksBlock';
 import TitleBlock from './components/TitleBlock';
 import LocationBlock from './components/LocationBlock';
 import NavBlock from './components/NavBlock';
+import { motion } from 'framer-motion';
 
 export default function Intro() {
   return (
-    <section
+    <motion.div
+      initial={{ opacity: 0, x: -10 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: 10 }}
+      transition={{ duration: 0.25 }}
       className="relative min-h-screen max-h-screen w-full p-5 md:p-10 bg-black grid gap-6 grid-rows-2"
       style={{ gridTemplateColumns: 'repeat(14, minmax(0, 1fr))' }}
     >
@@ -15,6 +20,6 @@ export default function Intro() {
       <LinksBlock />
       <LocationBlock />
       <NavBlock />
-    </section>
+    </motion.div>
   );
 }
