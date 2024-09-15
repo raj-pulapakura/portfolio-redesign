b0bc205950cbd2739a3f9c33416b8e3c2b856b9a
Raj <raj.pulapakura@gmail.com>
2024-09-15 19:25:32 +1000

    Add svg separators

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Journey from './sections/journey';
 import Outro from './sections/outro';
 import PreOutro from './sections/pre-outro';
 import Projects from './sections/projects';
+import Separator from './sections/separators/Separator';
 import Skills from './sections/skills';
 import Videos from './sections/videos';
 
@@ -11,7 +12,20 @@ function App() {
   return (
     <main>
       <Intro />
+      <Separator />
       <Skills />
+      <svg
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1200 120"
+        preserveAspectRatio="none"
+        className="bg-primary"
+      >
+        <path
+          className="fill-blue-200"
+          d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+        ></path>
+      </svg>
       <Journey />
       <Projects />
       <Articles />
--- src/sections/skills/index.tsx
+++ src/sections/skills/index.tsx
@@ -6,7 +6,7 @@ import SkillsBoard from './components/SkillsBoard';
 
 export default function Skills() {
   return (
-    <section className="w-full relative px-10 py-32 flex flex-col items-center justify-center bg-white">
+    <section className="w-full relative px-10 py-32 flex flex-col items-center justify-center bg-blue-200">
       <div id="skills" className="absolute top-0 left-0 w-full h-20"></div>
 
       <div className="w-10/12 mx-auto flex flex-row gap-32 items-center">
